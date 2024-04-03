fd8fcd6a32e1e3db15b930611f9e3703516214e3
Harry San Juan <harrycsanjuan@gmail.com>
2024-04-03 01:40:45 -0400

    completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,31 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, dwarf in enumerate(dwarves, start= 1):
+        print(f"{index}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    summon_capt = [element.title() + "!" for element in planeteer_calls]
+    return summon_capt
+        
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for word in calls:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+# First try
+# def find_the_cheese(ingredients):
+#     for item in ingredients:
+#         cheeses = ["cheddar", "gouda", "camembert"]
+#         for cheese in cheeses:
+#             if item == cheese:
+#                 return item
+#     return None
+
+# Refactored
+def find_the_cheese(ingredients):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if ingredient in cheeses:
+            return ingredient
+    return None
